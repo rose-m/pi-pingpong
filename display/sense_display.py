d0f42fa1d18cae5a1d41ca89e1fe32442399b48b
rosem <michael_rose@gmx.de>
2019-01-10 15:03:08 +0100

    fix display of message on sense_hat

--- display/sense_display.py
+++ display/sense_display.py
@@ -1,3 +1,6 @@
+from threading import Thread
+from time import sleep
+
 from sense_hat import SenseHat
 
 from display.color import Color
@@ -5,26 +8,41 @@ from display.display import Display
 
 
 class SenseDisplay(Display):
-    SCROLL_SPEED = 1.0
+    SCROLL_SPEED = 0.4
 
     def __init__(self):
         super().__init__()
         self._sense = None
+        self._message = None
+        self._color = None
+        self._thread = None
+        self._running = True
 
-    def init(self):
+    def init(self) -> None:
         self._sense = SenseHat()
+        self._thread = Thread(target=self._render_message)
+        self._thread.start()
 
-    def clear(self):
+    def clear(self) -> None:
+        self._running = False
+        self._thread.join(timeout=5)
         self._sense.clear()
 
     def show_message(self, message: str, color: Color = Color.DEFAULT) -> None:
-        self._sense.show_message(message, SenseDisplay.SCROLL_SPEED, self._convert_color(color))
+        self._message = message
+        self._color = color
 
     def show_timer(self, seconds_remaining: int, message: str = None, color: Color = Color.DEFAULT) -> None:
         self.show_message(
             '%s - %02d:%02d' % (message, seconds_remaining // 60, seconds_remaining % 60),
             color
         )
+
+    def _render_message(self) -> None:
+        while self._running:
+            if self._message is not None:
+                self._sense.show_message(self._message, SenseDisplay.SCROLL_SPEED, self._convert_color(self._color))
+            sleep(0.5)
 
     @staticmethod
     def _convert_color(color: Color) -> [int, int, int]:
